62a0bf74909f21f4563548bb011cc478693e0b7e
abutalha08 <taufique752@gmail.com>
2026-05-12 17:14:00 +0600

    Access client data in POST API to save Data to Server

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 8000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-
-    res.send('Hello from users world');
-})
+app.get("/", (req, res) => {
+  res.send("Hello from users world");
+});
 
 const users = [
-    { id: 1, name: 'John Doe', email: 'john@example.com' },
-    { id: 2, name: 'Jane Doe', email: 'jane@example.com' },
-    { id: 3, name: 'Jim Doe', email: 'jim@example.com' }
+  { id: 1, name: "John Doe", email: "john@example.com" },
+  { id: 2, name: "Jane Doe", email: "jane@example.com" },
+  { id: 3, name: "Jim Doe", email: "jim@example.com" },
 ];
 
-app.get('/users', (req, res) => {
-    // res.send('Users are waking up');
-    res.send(users);
-})
+app.get("/users", (req, res) => {
+  // res.send('Users are waking up');
+  res.send(users);
+});
 
-app.get('/products', (req, res) => {
-    res.send('Products are showing here');
-})
+app.post("/users", (req, res) => {
+  console.log("data in the request:", req.body);
 
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
 
+  res.send({
+    success: true,
+    data: newUser,
+    message: "User added successfully",
+  });
+});
 
+app.get("/products", (req, res) => {
+  res.send("Products are showing here");
+});
 
-app.listen(port, ()=> {
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
