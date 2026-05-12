b4d2cacbcda2d03d5e757cfc5079e28fc236ea10
abutalha08 <taufique752@gmail.com>
2026-05-12 15:35:20 +0600

    Install Express and Creating my  first API GET method

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ app.get('/', (req, res) => {
     res.send('Hello from users world');
 })
 
+const users = [
+    { id: 1, name: 'John Doe', email: 'john@example.com' },
+    { id: 2, name: 'Jane Doe', email: 'jane@example.com' },
+    { id: 3, name: 'Jim Doe', email: 'jim@example.com' }
+];
+
+app.get('/users', (req, res) => {
+    // res.send('Users are waking up');
+    res.send(users);
+})
+
+app.get('/products', (req, res) => {
+    res.send('Products are showing here');
+})
+
+
+
 
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`);
